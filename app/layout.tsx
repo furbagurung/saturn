bcd49043c63da47cb9d6c9f6dd518a947fd0ca38
furbagurung <foorvaa.grg@gmail.com>
2026-01-06 05:41:19 +0545

    intial loader removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import InitialLoader from "@/components/intial-loader";
+
 
 
 const geistSans = Geist({
@@ -32,7 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-  <InitialLoader ms={1200}>{children}</InitialLoader>
+{children}
       </body>
     </html>
   );
